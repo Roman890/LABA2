d29d64ce84019fafdbcf1279f4fc36af79403aab
Roman890 <rom.nosov2014@yandex.ru>
2017-03-09 22:23:36 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,176 @@
+
+#include <iostream>
+#include <cstring>
+#include <math.h>
+#include <cstdlib>
+
+using namespace std;
+const int MAX = 10;     
+
+
+class Stack
+{
+private:
+	char action[MAX];         // массив для символов действий
+	int top1;                 // вершина для символьного массива
+	int number[MAX];          // массив для чисел
+	int top2;                 // вершина для числового массива
+public:
+	Stack()              // конструктор
+	{
+		top1 = 0;
+		top2 = 0;
+	}
+	void pushAction(char var) // поместить в символьный массив cимвол
+	{
+		action[top1] = var;
+		top1++;
+	}
+	char popAction()           // взять из символьного массива символ
+	{
+		top1--;
+		return action[top1];
+	}
+	int gettopAction()         // узнать количество элементов в массиве символов
+	{
+		return top1;
+	}
+	int gettopNumber()         // узнать количество элементов в массиве символов
+	{
+		return top2;
+	}
+	void pushNumber(int var)  // поместить в символьный массив cимвол
+	{
+		number[top2] = var;
+		top2++;
+	}
+	int popNumber()           // взять из символьного массива символ
+	{
+		top2--;
+		return number[top2];
+	}
+	int sum()
+	{
+		return number[top2-2] = number[top2 - 2] + number[top2-1];
+	}
+	int minus()
+	{
+		return number[top2-2] = number[top2 - 2] - number[top2-1];
+	}
+	int divid()
+	{
+		return number[top2-2] = number[top2 - 2] / number[top2-1];
+	}
+	int multy()
+	{
+		return number[top2-2] = number[top2 - 2] * number[top2 - 1];
+	}
+	int stepen()
+	{
+		return number[top2-2] = pow(number[top2 - 2], number[top2-1]); 
+	}
+	char popActionTop1_1()          
+	{
+		return action[top1-1];
+	}
+};
+
+
+class CALCULATOR
+{
+private:
+	Stack s;                 
+	char* pStr;                // строка для ввода
+	int len;                   // длина строки
+public:
+	CALCULATOR(char* ptr)      // конструктор
+	{
+		pStr = ptr;            // запоминаем указатель на строку
+		len = strlen(pStr);    // устанавливаем длину
+	}
+	int calc(char* str, int status);  // алгоритм выполнения действий
+	void action(char act);             // выполнение действий
+	int prioritet(char act);           // определение приоритета действия
+};
+
+
+
+void CALCULATOR::action(char act) {
+	switch (act)
+	{
+	case '+': s.pushNumber(s.sum()); break;
+	case '-': s.pushNumber(s.minus()); break;
+	case '*': s.pushNumber(s.multy()); break;
+	case '/': s.pushNumber(s.divid()); break;
+	case '^': s.pushNumber(s.stepen()); break;
+	default: cout << "\nНеизвестный оператор"; exit(1);
+	}
+	return;
+}
+
+int CALCULATOR::prioritet(char act)
+{
+	switch (act)
+	{
+	case '+': return 1; break;
+	case '-': return 1; break;
+	case '*': return 2; break;
+	case '/': return 2; break;
+	case '^': return 3; break;
+	}
+}
+
+int CALCULATOR::calc(char* string, int status)
+{
+	int l = strlen(string);
+	char n;
+	int k;
+
+	// заполнение двух стеков: для действий и для чисел 
+	for (int i = 0; i < l; ++i)
+	{
+		// если число, то заполняем стек для чисел
+		if ((string[i] <= '9') && (string[i] >= '0'))
+		{
+			s.pushNumber(atof(string + i));
+		}
+		// если приоритет операции ниже или равен операции, находящейся на вершине, то  
+		else if (prioritet(s.popActionTop1_1()) >= prioritet(string[i]))
+		{
+			n = s.popAction();   //выталкиваем операцию из стека
+			action(n);           //выполняем действие со стеком из чисел 
+			s.pushAction(string[i]);//ставим новую операцию на стек  действий
+		}
+		// если приоритет операции выше, то заполняем стек для действий 
+		else
+		{
+		 s.pushAction(string[i]);
+		}
+	}
+	while (1)
+	{
+
+		if (s.gettopAction() == 0) { break; }
+		n = s.popAction();
+		cout << n << endl;
+		action(n);
+	}
+	return s.popNumber();
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	char string[MAX]; 
+
+	cout << "\nВведите арифметическое выражение:  ";
+	cin >> string;
+	CALCULATOR* task = new CALCULATOR(string);					// создаем объект для разбора
+	cout << "\nРезультат: " << task->calc(string,1) << endl;    // решаем
+	delete task;												// удаляем объект
+	
+	return 0;
+}
+
+
